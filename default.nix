c1affed7ad9b513e89997af0edb0f2cea706a22b
Gavin Rohrer <gavin@rohrer.org>
2024-08-24 01:57:20 -0400

    fixup default.nix

--- default.nix
+++ default.nix
@@ -1,18 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.rustPlatform.buildRustPackage rec {
+{ pkgs ? import <nixpkgs> {} }: with pkgs;
+rustPlatform.buildRustPackage rec {
   pname = "mkproj";
   version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "gfaster";
     repo = pname;
-    rev = version;
-    sha256 = "";
+    rev = "v${version}";
+    sha256 = "sha256-NtkEM+uVG2gp6XjdqgRX5Dv5Aup8BQZCaV07IztxsTo=";
   };
 
-  cargoSha256 = "";
+  cargoSha256 = "sha256-sn4lbWSd47wWU+wC9SS8VJ4Obgqo1uZZTZ/PWJ4BjwM=";
 
-  meta = with stdenv.lib; {
+  meta = with lib; {
     description = "My builder for creating new projects";
     homepage = "https://github.com/gfaster/mkproj";
     license = licenses.gpl3Plus;
